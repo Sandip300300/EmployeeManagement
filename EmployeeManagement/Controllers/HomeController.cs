765ed709ebdfa4be14d1839a7b2b77e0aeaf7f46
Sandip300300 <sandip300300@gmail.com>
2024-02-27 21:04:00 +0600

    Add project files.

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(ILogger<HomeController> logger,IEmployeeRepository employeeRepository)
+        {
+            _logger = logger;
+            _employeeRepository = employeeRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployee();
+            return View(model);
+        }
+
+        public IActionResult Details()
+        {
+            HomeDetailsViewModel viewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(1),
+                PageTitle = "Employee Details"
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ EmployeeManagement/Models/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace EmployeeManagement.Models
+{
+    public interface IEmployeeRepository
+    {
+        Employee GetEmployee(int id);
+        IEnumerable<Employee> GetAllEmployee();
+    }
+}
